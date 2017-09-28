a46300fb7e326eedf8a7df70752875ae35134976
collette-tamez <cttmz4@gmail.com>
2017-09-28 16:37:22 -0700

    complete feature set 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const $root = document.getElementById('root');
+const user = {
+  name: 'Max',
+}
+user.luckyNumber = luckyNumber();
+
+function luckyNumber() {
+  let rndmNum = Math.floor(Math.random()*1000) + 1;
+  return rndmNum;
+}
+
+const greeting = (obj) => (
+  <div className="container">
+    <h1>wassup {obj.name}</h1>
+    <h2 className="text-muted">Your Lucky Number is {obj.luckyNumber}</h2>
+  </div>
+)
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
+    //<h1 className="container">React Prototypes!</h1>,
+    greeting(user),
+    $root
 );
