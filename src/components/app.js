560b1ff68540c96cc6c5cf675b6df6a0c05c8969
collette-tamez <cttmz4@gmail.com>
2017-12-08 14:33:07 -0800

    completed prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Table from './components/table';
+import Table from './table';
+
+const students = [
+  {
+    student: "Holder",
+    course: "kisses",
+    grade: 100
+  },
+  {
+    student: "Shrody",
+    course: "biting",
+    grade: 100
+  },
+  {
+    student: "Catlus",
+    course: "cat wheel running",
+    grade: 100
+  }
+];
+
 
 function App(){
   return (
-    <h1>Muh first React functionl component or sumthin</h1>
-    <Table />
+    //<h1>Muh first React functionl component or sumthin</h1>
+    <div className={'container'}>
+      <h1>SGT</h1>
+      <Table data = {students} />
+    </div>
   )
 }
 
--- src/components/table.js
+++ src/components/table.js
@@ -1,29 +1,26 @@
 import React from 'react';
 
-function Table (){
+function Table (props){
+    const tableRows = props.data.map((item, index)=>{
+        return (
+            <tr key={index}>
+                <td>{item.student}</td>
+                <td>{item.course}</td>
+                <td>{item.grade}</td>
+            </tr>
+        )
+    });
     return (
         <table className="table" >
             <thead className="thead-inverse">
-                <th>Name</th>
-                <th>Course</th>
-                <th>Grade</th>
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>cats</td>
-                    <td>food</td>
-                    <td>98</td>
-                </tr>
-                <tr>
-                    <td>cats1</td>
-                    <td>food</td>
-                    <td>99</td>
-                </tr>
-                <tr>
-                    <td>cats2</td>
-                    <td>food</td>
-                    <td>100</td>
-                </tr>
+                {tableRows}
             </tbody>
         </table>
     )
