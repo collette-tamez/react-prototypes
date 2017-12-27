e094d61766afb5dd4b620ad183ee7729af85df69
collette-tamez <cttmz4@gmail.com>
2017-12-27 15:45:49 -0800

    feature complete

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -26,6 +26,7 @@ class ContactFrom extends Component {
     handleSubmit(event){
         event.preventDefault();
         console.log(this.state.form);
+        this.props.add(this.state.form);
     }
     render(){
         const {email, firstName, lastName, phone} = this.state.form;
