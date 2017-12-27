fa1eff1dae94ba5d994c8de900bf9e32dcf707b8
collette-tamez <cttmz4@gmail.com>
2017-12-27 13:51:00 -0800

    completed feature set 2

---
+++ src/data/contacts.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        firstName: "Holder",
+        lastName: "Sinocruz",
+        phone: "2-222-2222",
+        email: "topdog@holdersworld.me"
+    },
+    {
+        firstName: "Cesca",
+        lastName: "Sinocruz",
+        phone: "2-22-22222222",
+        email: "holdinholder@holdersworld.me"
+    },
+    {
+        firstName: "Max",
+        lastName: "Beats",
+        phone: "111111111",
+        email: "security@holdersworld.me"
+    },
+    {
+        firstName: "Zombo",
+        lastName: "Com",
+        phone: "4444444444",
+        email: "zombocom@zombocom.com"
+    }
+];
