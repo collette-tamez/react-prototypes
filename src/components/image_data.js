b9ed68800e6200d7497a4f7fef2fbf64935d6932
collette-tamez <cttmz4@gmail.com>
2018-03-29 11:48:48 -0700

    feature complete:

---
+++ src/components/image_data.js
@@ -0,0 +1,72 @@
+import kiss from './images/kiss.jpg';
+import kids from './images/kids.png';
+import banana from './images/banana.png';
+import wink from './images/wink.png';
+import fancy from './images/fancy1.png';
+import fluke from './images/fluke1.png';
+import shocked from './images/shocked1.png';
+import sonic from './images/unknown.png';
+
+export default [
+    {
+        src: kiss,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: kids,
+        style: {
+            left:'40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: banana,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: wink,
+        style: {
+            left: '8%',
+            top: '30%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: fancy,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: fluke,
+        style: {
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(10deg)'
+        }
+    },
+    {
+        src: shocked,
+        style: {
+            left: '25%',
+            top: '48%',
+            transform: 'rotate(-5deg)'
+        }
+    },
+    {
+        src: sonic,
+        style: {
+            right: '25%',
+            top: '52%',
+            transform: 'rotate(16deg)'
+        }
+    }
+];
---
+++ src/components/scrapbook.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../src/components/scrapbook.css';
+import imageData from './image_data';
+import ScrapbookImage from './scrapbook_images';
+
+export default () => {
+    const images = imageData.map((item, index)=>{
+        return <ScrapbookImage key={index} about={item}/>
+    });
+    return ( 
+        <div className={"scrapbook-container"}>
+            {images}
+        </div>
+    )
+}
